b0f9aa146982c79563974a56efd2669702134a77
Adam Baker <adam.baker.kp@hotmail.com>
2024-03-25 11:17:54 +1100

    Creation Update #1
    pre added player slots in an array which will hold their totals (all time stats in every game) and their player images.
    Coded basic setup for index page and setup the css stylesheet.

---
+++ js/kpstats.js
@@ -0,0 +1,453 @@
+const player = [
+	{ 	/* 1 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/01.png"
+	},
+	
+	{	/* 2 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/02.png"
+	}
+	
+	{	/* 3 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/03.png"
+	}
+	
+	{	/* 4 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/04.png"
+	}
+	
+	{	/* 5 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/05.png"
+	}
+	
+	{	/* 6 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/06.png"
+	}
+	
+	{	/* 7 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/07.png"
+	}
+	
+	{	/* 8 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/08.png"
+	}
+	
+	{	/* 9 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/09.png"
+	}
+	
+	{	/* 10 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/10.png"
+	}
+	
+	{	/* 11 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/11.png"
+	}
+	
+	{	/* 12 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/12.png"
+	}
+	
+	{	/* 13 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/13.png"
+	}
+	
+	{	/* 14 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/14.png"
+	}
+	
+	{	/* 15 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/15.png"
+	}
+	
+	{	/* 16 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/16.png"
+	}
+	
+	{	/* 17 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/17.png"
+	}
+	
+	{	/* 18 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/18.png"
+	}
+	
+	{	/* 19 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/19.png"
+	}
+	
+	{	/* 20 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/20.png"
+	}
+	
+	{	/* 21 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/21.png"
+	}
+	
+	{	/* 22 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/22.png"
+	}
+	
+	{	/* 23 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/23.png"
+	}
+	
+	{	/* 24 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/24.png"
+	}
+	
+	{	/* 25 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/25.png"
+	}
+	
+	{	/* 26 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/26.png"
+	}
+	
+	{	/* 27 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/27.png"
+	}
+	
+	{	/* 28 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/28.png"
+	}
+	
+	{	/* 29 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/29.png"
+	}
+	
+	{	/* 30 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/30.png"
+	}
+	
+	{	/* 31 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/31.png"
+	}
+	
+	{	/* 32 */
+		fName:"", sName:"",
+		tDisposals: 0,
+		tKicks: 0,
+		tHandballs: 0,
+		tClearances: 0,
+		tTackles: 0,
+		tMarks: 0,
+		tGoals: 0,
+		tBehinds: 0,
+		tFantasy: 0,
+		pImage: "img/player_jerseys/32.png"
+	}
+];
+
+function loadDefault() {
+	//console.log("loaded");
+}
